be2b36d32cf72c2bc6c56e32d037f68ee01a7c2b
AlphaHalcyon <74324748+AlphaHalcyon@users.noreply.github.com>
2023-03-25 17:52:05 -0400

    Initial Commit

---
+++ Starhaven/StarhavenApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarhavenApp.swift
+//  Starhaven
+//
+//  Created by JxR on 3/25/23.
+//
+
+import SwiftUI
+
+@main
+struct StarhavenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
