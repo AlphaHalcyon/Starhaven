c17b16fd0e27743571b81bccb56a92fdce436745
AlphaHalcyon <74324748+AlphaHalcyon@users.noreply.github.com>
2023-04-13 21:25:09 -0400

    Settled on warhead animation.

--- Starhaven/Missile.swift
+++ Starhaven/Missile.swift
@@ -13,7 +13,7 @@ class Missile {
     var missileNode: SCNNode
     var target: SCNNode?
     var redParticleSystem: SCNParticleSystem
-
+    var explosionNode: SCNNode = SCNNode()
     init(target: SCNNode? = nil) {
         self.target = target
 
@@ -28,7 +28,7 @@ class Missile {
         // Create red particle system
         redParticleSystem = SCNParticleSystem()
         redParticleSystem.particleColor = UIColor.red
-        redParticleSystem.particleSize = 0.005
+        redParticleSystem.particleSize = 0.00725
         redParticleSystem.birthRate = 10000
         redParticleSystem.emissionDuration = 1
         redParticleSystem.particleLifeSpan = 4
@@ -37,7 +37,7 @@ class Missile {
 
         // Attach red particle system to the tail of the missile
         let emitterNode = SCNNode()
-        emitterNode.position = SCNVector3(0, -0.25, 0)
+        emitterNode.position = SCNVector3(0, -1, 0)
         emitterNode.addParticleSystem(redParticleSystem)
         missileNode.addChildNode(emitterNode)
         self.missileNode.physicsBody?.isAffectedByGravity = false
@@ -45,29 +45,40 @@ class Missile {
         self.missileNode.physicsBody?.damping = 0
         let detonationTimer = Timer.scheduledTimer(withTimeInterval: 7, repeats: false) { _ in
             self.detonate()
+            print("boom")
+            
         }
     }
-    var timeSinceFired: TimeInterval = 0
-    let detonationTime: TimeInterval = 7
     func handleCollision() {
         detonate()
     }
     func detonate() {
-        // Create a particle system for the explosion
-        let explosion = SCNParticleSystem()
-        explosion.emitterShape = SCNSphere(radius: 50)
-        explosion.birthRate = 500000
-        explosion.particleLifeSpan = 0.5
-        explosion.particleVelocity = 10
-        explosion.particleVelocityVariation = 5
-        explosion.particleSize = 0.05
-        explosion.stretchFactor = 0.05
-
-        // Add the particle system to the missile node
-        missileNode.addParticleSystem(explosion)
-
+        let coronaGeo = SCNSphere(radius: 50)
+        
+        // Create the particle system programmatically
+        let fireParticleSystem = SCNParticleSystem()
+        fireParticleSystem.particleImage = UIImage(named: "SceneKit Asset Catalog.scnassets/SunWeakMesh.jpg")
+        fireParticleSystem.birthRate = 500000
+        fireParticleSystem.particleSize = 0.75
+        fireParticleSystem.particleIntensity = 1
+        fireParticleSystem.particleLifeSpan = 0.5
+        fireParticleSystem.spreadingAngle = 180
+        fireParticleSystem.particleAngularVelocity = 50
+        fireParticleSystem.emitterShape = coronaGeo
+        // Make the particle system surface-based
+        fireParticleSystem.emissionDurationVariation = fireParticleSystem.emissionDuration
+        // Add the particle system to the warhead
+        
+        
+        let implodeAction = SCNAction.scale(to: 10, duration: 0.5)
+        let implodeActionStep = SCNAction.scale(to: 5, duration: 0.25)
+        let implodeActionEnd = SCNAction.scale(to: 0.1, duration: 0.125)
+        let pulseSequence = SCNAction.sequence([implodeAction, implodeActionStep, implodeActionEnd])
+        self.explosionNode.addParticleSystem(fireParticleSystem)
+        self.missileNode.addChildNode(self.explosionNode)
+        self.explosionNode.runAction(SCNAction.repeat(pulseSequence, count: 1))
         // Remove the missile node from the scene after a delay
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
             self.missileNode.removeFromParentNode()
         }
     }
